591ee8efd2b50bd5fca0e4518f9ee28f9fbff99d
ariwahidin <37979597+ariwahidin@users.noreply.github.com>
2024-03-19 20:29:12 +0700

    custom login page

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -10,7 +10,8 @@
     <meta content="Themesbrand" name="author" />
     <!-- App favicon -->
     <link rel="shortcut icon" href="<?= base_url('jar/html/default/') ?>assets/images/yusen-kotak.jpg">
-
+    <!--Swiper slider css-->
+    <link href="<?= base_url('jar/html/default/') ?>assets/libs/swiper/swiper-bundle.min.css" rel="stylesheet" type="text/css" />
     <!-- Layout config Js -->
     <script src="<?= base_url('jar/html/default/') ?>assets/js/layout.js"></script>
     <!-- Bootstrap Css -->
@@ -41,53 +42,54 @@
                         <div class="card overflow-hidden">
                             <div class="row g-0">
                                 <div class="col-lg-6">
-                                    <div class="p-lg-5 p-4 auth-one-bg h-100">
+                                    <div class="swiper default-swiper rounded swiper-initialized swiper-horizontal swiper-backface-hidden">
+                                        <div class="swiper-wrapper" id="" aria-live="off">
+                                            <div class="swiper-slide swiper-slide-active" role="group" data-swiper-slide-index="1" style="width: 400px;">
+                                                <img src="<?= base_url('jar/html/default/') ?>assets/images/small/aerox.png" alt="" class="img-fluid">
+                                            </div>
+                                            <div class="swiper-slide swiper-slide-next" role="group" data-swiper-slide-index="2" style="width: 400px;">
+                                                <img src="<?= base_url('jar/html/default/') ?>assets/images/small/xsr.png" alt="" class="img-fluid">
+                                            </div>
+                                            <div class="swiper-slide swiper-slide-next" role="group" data-swiper-slide-index="2" style="width: 400px;">
+                                                <img src="<?= base_url('jar/html/default/') ?>assets/images/small/mxking.png" alt="" class="img-fluid">
+                                            </div>
+                                            <div class="swiper-slide swiper-slide-next" role="group" data-swiper-slide-index="3" style="width: 400px;">
+                                                <img src="<?= base_url('jar/html/default/') ?>assets/images/small/xmax.png" alt="" class="img-fluid">
+                                            </div>
+                                            <div class="swiper-slide swiper-slide-next" role="group" data-swiper-slide-index="0" style="width: 400px;">
+                                                <img src="<?= base_url('jar/html/default/') ?>assets/images/small/nmax.png" alt="" class="img-fluid">
+                                            </div>
+                                            <div class="swiper-slide swiper-slide-next" role="group" data-swiper-slide-index="0" style="width: 400px;">
+                                                <img src="<?= base_url('jar/html/default/') ?>assets/images/small/filano.png" alt="" class="img-fluid">
+                                            </div>
+                                        </div>
+                                        <!-- <span class="swiper-notification" aria-live="assertive" aria-atomic="true"></span> -->
+                                    </div>
+                                    <!-- <div class="p-lg-5 p-4 auth-one-bg h-100">
                                         <div class="bg-overlay"></div>
                                         <div class="position-relative h-100 d-flex flex-column">
                                             <div class="mb-4">
                                                 <a href="index.html" class="d-block">
                                                     <h3 class="text-white mb-1">
-                                                        <img id="logo" src="<?= base_url('jar/html/default/') ?>assets/images/yusen-kotak.jpg" alt="" height="70">
+                                                        <img id="logo" src="<?= base_url('jar/html/default/') ?>assets/images/yusen-kotak.jpg" alt="" height="60">
                                                         <span style="text-decoration: underline;">Yamaha Motor Indonesia</span>
                                                     </h3>
                                                 </a>
                                             </div>
                                             <div class="mt-auto">
-                                                <!-- <div class="mb-3">
-                                                    <i class="ri-double-quotes-l display-4 text-success"></i>
-                                                </div> -->
-
-                                                <!-- <div id="qoutescarouselIndicators" class="carousel slide" data-bs-ride="carousel">
-                                                    <div class="carousel-indicators">
-                                                        <button type="button" data-bs-target="#qoutescarouselIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                                                        <button type="button" data-bs-target="#qoutescarouselIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                                                        <button type="button" data-bs-target="#qoutescarouselIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                                                    </div>
-                                                    <div class="carousel-inner text-center text-white-50 pb-5">
-                                                        <div class="carousel-item active">
-                                                            <p class="fs-15 fst-italic">" Great! Clean code, clean design, easy for customization. Thanks very much! "</p>
-                                                        </div>
-                                                        <div class="carousel-item">
-                                                            <p class="fs-15 fst-italic">" The theme is really great with an amazing customer support."</p>
-                                                        </div>
-                                                        <div class="carousel-item">
-                                                            <p class="fs-15 fst-italic">" Great! Clean code, clean design, easy for customization. Thanks very much! "</p>
-                                                        </div>
-                                                    </div>
-                                                </div> -->
-                                                <!-- end carousel -->
+                                                
                                             </div>
                                         </div>
-                                    </div>
+                                    </div> -->
                                 </div>
                                 <!-- end col -->
 
                                 <div class="col-lg-6">
                                     <div class="p-lg-5 p-4">
                                         <div>
-
-                                            <h5 class="text-primary">Welcome!</h5>
-                                            <p class="text-muted">Sign in to continue.</p>
+                                            <img id="logo" src="<?= base_url('jar/html/default/') ?>assets/images/small/yusen-logistics.png" alt="" height="60">
+                                            <!-- <h5 class="text-primary">Indonesia</h5> -->
+                                            <!-- <p class="text-muted">Indonesia.</p> -->
                                         </div>
 
                                         <div class="mt-4">
@@ -171,6 +173,12 @@
     <script src="<?= base_url('jar/html/default/') ?>assets/libs/feather-icons/feather.min.js"></script>
     <script src="<?= base_url('jar/html/default/') ?>assets/js/pages/plugins/lord-icon-2.1.0.js"></script>
     <script src="<?= base_url('jar/html/default/') ?>assets/js/plugins.js"></script>
+
+    <!--Swiper slider js-->
+    <script src="<?= base_url('jar/html/default/') ?>assets/libs/swiper/swiper-bundle.min.js"></script>
+
+    <!-- swiper.init js -->
+    <script src="<?= base_url('jar/html/default/') ?>assets/js/pages/swiper.init.js"></script>
 
     <!-- password-addon init -->
     <script src="<?= base_url('jar/html/default/') ?>assets/js/pages/password-addon.init.js"></script>
